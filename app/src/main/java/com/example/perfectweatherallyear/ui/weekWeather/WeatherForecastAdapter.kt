9ba40dc33f6e5c1d5d230764b181fc16e00c7717
polina <p.shcherbinina@itransition.com>
2021-10-20 16:40:54 +0300

    recyclerView - clicks on recyclerView(fixes)

--- app/src/main/java/com/example/perfectweatherallyear/ui/weekWeather/WeatherForecastAdapter.kt
+++ app/src/main/java/com/example/perfectweatherallyear/ui/weekWeather/WeatherForecastAdapter.kt
@@ -1,15 +1,15 @@
 package com.example.perfectweatherallyear.ui.weekWeather
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.perfectweatherallyear.databinding.WeatherRowItemBinding
 import com.example.perfectweatherallyear.model.DayWeather
 
-class WeatherForecastAdapter(private val weekWeatherMap: Map<String, DayWeather>, private val onClick: (DayWeather) -> Unit)
-    : RecyclerView.Adapter<WeatherForecastAdapter.ViewHolder>() {
-    var dayWeather: DayWeather? = null
+class WeatherForecastAdapter(
+    private val weekWeatherMap: Map<String, DayWeather>,
+    private val onClick: (DayWeather) -> Unit
+) : RecyclerView.Adapter<WeatherForecastAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val binding = WeatherRowItemBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
@@ -17,18 +17,23 @@ class WeatherForecastAdapter(private val weekWeatherMap: Map<String, DayWeather>
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        viewHolder.binding.apply {
-            dayWeather = weekWeatherMap.get(weekWeatherMap.keys.toTypedArray()[position])
-            dayOfWeekTextView.text = weekWeatherMap.keys.elementAt(position)
-            maxMinTempretureTextView.text =   dayWeather?.temperature
-        }
-        dayWeather?.let { viewHolder.bind(it) }
+            getDayWeather(position).also {
+                viewHolder.bind(it)
+            }
     }
 
     override fun getItemCount() = weekWeatherMap.size
 
-    inner class ViewHolder(val binding: WeatherRowItemBinding, val onClick: (DayWeather) -> Unit) : RecyclerView.ViewHolder(binding.root),
-        View.OnClickListener {
+    private fun getDayWeather(position: Int): Pair<String, DayWeather> {
+        return weekWeatherMap.entries.toTypedArray()[position].let {
+            Pair(it.key, it.value)
+        }
+    }
+
+    inner class ViewHolder(
+        val binding: WeatherRowItemBinding,
+        val onClick: (DayWeather) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
         private var currentDayWeather: DayWeather? = null
 
         init {
@@ -39,11 +44,12 @@ class WeatherForecastAdapter(private val weekWeatherMap: Map<String, DayWeather>
             }
         }
 
-        fun bind(dayWeather: DayWeather) {
-            currentDayWeather = dayWeather
-        }
-
-        override fun onClick(view: View?) {
+        fun bind(dayWeather: Pair<String, DayWeather>) {
+            currentDayWeather = dayWeather.second
+            binding.apply {
+                dayOfWeekTextView.text = dayWeather.first
+                maxMinTempretureTextView.text = dayWeather.second.temperature
+            }
         }
     }
 }
--- app/src/main/java/com/example/perfectweatherallyear/ui/weekWeather/WeekWeatherFragment.kt
+++ app/src/main/java/com/example/perfectweatherallyear/ui/weekWeather/WeekWeatherFragment.kt
@@ -19,9 +19,12 @@ class WeekWeatherFragment : Fragment(){
 
     private val weekWeatherMap = mapOf(
         "Monday" to DayWeather("+15/+5", 15, 5),
-        "Tuesday" to DayWeather("+15/+5", 15, 5), "Wednesday" to DayWeather("+16/+6", 20, 10),
-        "Thursday" to DayWeather("+20/+15", 70, 2), "Friday" to DayWeather("+15/+7", 10, 15),
-        "Saturday" to DayWeather("+10/+5", 20, 5), "Sunday" to DayWeather("+7/+0", 15, 8)
+        "Tuesday" to DayWeather("+15/+5", 15, 5),
+        "Wednesday" to DayWeather("+16/+6", 20, 10),
+        "Thursday" to DayWeather("+20/+15", 70, 2),
+        "Friday" to DayWeather("+15/+7", 10, 15),
+        "Saturday" to DayWeather("+10/+5", 20, 5),
+        "Sunday" to DayWeather("+7/+0", 15, 8)
     )
 
     override fun onCreateView(
