fb2fe08ae836737ea68130125dc426b9e7bcefd6
polinas888 <p.shcherbinina@itransition.com>
2022-05-26 10:14:34 +0300

    WorkManager.Update hour weather on background and send notification

--- app/src/main/java/com/example/perfectweatherallyear/util/WorkerHandler.kt
+++ app/src/main/java/com/example/perfectweatherallyear/util/WorkerHandler.kt
@@ -4,18 +4,24 @@ import android.content.Context
 import androidx.work.*
 import androidx.work.ListenableWorker.Result.success
 import com.example.perfectweatherallyear.appComponent
+import com.example.perfectweatherallyear.model.DayWeather
+import com.example.perfectweatherallyear.model.HourWeather
 import com.example.perfectweatherallyear.model.Location
 import com.example.perfectweatherallyear.repository.DataResult
 import com.example.perfectweatherallyear.repository.WeatherRepository
 import com.example.perfectweatherallyear.ui.weekWeather.DAYS_NUMBER
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class WorkerHandler(appContext: Context, workerParams: WorkerParameters): Worker(appContext, workerParams) {
     @Inject
     lateinit var repository: WeatherRepository
+    lateinit var dayWeatherForNotification: DayWeather
+    lateinit var hourWeatherForNotification:  HourWeather
 
     init {
         appContext.appComponent.inject(this)
@@ -23,9 +29,8 @@ class WorkerHandler(appContext: Context, workerParams: WorkerParameters): Worker
 
     override fun doWork(): Result {
         GlobalScope.launch {
-            NotificationHandler.createNotification(applicationContext, loadForecastForNotification())
+            NotificationHandler.createNotification(applicationContext, loadHourWeatherTempForNotification())
         }
-
         return success()
     }
 
@@ -46,18 +51,34 @@ class WorkerHandler(appContext: Context, workerParams: WorkerParameters): Worker
         }
     }
 
-    private suspend fun loadForecastForNotification(): String {
-        var lastWeather = ""
+    private suspend fun loadDayWeatherForNotification(): DayWeather {
             val location = Location(1, "Moscow")
-            when (val weatherForecast = repository.getWeatherForecast(location, DAYS_NUMBER)) {
+          when (val weatherForecast = repository.getWeatherForecast(location, DAYS_NUMBER)) {
                 is DataResult.Ok -> {
-                    lastWeather = "max temp ${weatherForecast.response[weatherForecast.response.size - 1]
-                        .generalWeather.temperatureMax} min temp ${weatherForecast.response[weatherForecast.
-                    response.size - 1].generalWeather.temperatureMin}"
+                    dayWeatherForNotification = weatherForecast.response[weatherForecast.response.size - 1]
                 }
                 is DataResult.Error ->
                     weatherForecast.error
             }
-        return lastWeather
+        return dayWeatherForNotification
+    }
+
+    private suspend fun loadHourWeatherTempForNotification(): String {
+        var hourWeatherTempForNotification = ""
+            when (val hourlyWeatherForecast = repository.getHourlyWeather(DAYS_NUMBER, loadDayWeatherForNotification())) {
+                is DataResult.Ok -> {
+                    val sdf = SimpleDateFormat("HH:mm:ss")
+                    val currentHour: String = sdf.format(Date()).take(2)
+                    for (hourWeather in hourlyWeatherForecast.response) {
+                        val hour = hourWeather.time.substringAfterLast(" ").take(2)
+                        if(hour == currentHour) {
+                            hourWeatherForNotification = hourWeather
+                        }
+                    }
+                    hourWeatherTempForNotification = "temperature ${hourWeatherForNotification.temperature}"
+                }
+                is DataResult.Error -> hourlyWeatherForecast.error
+            }
+        return hourWeatherTempForNotification
     }
 }
