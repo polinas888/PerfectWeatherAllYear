cf4ab7837f1785c7c0c6f4110a62f41ac84d5a38
polinas888 <p.shcherbinina@itransition.com>
2022-05-30 12:07:59 +0300

    Sent broadcast from other app with long method with args

--- app/src/main/java/com/example/perfectweatherallyear/util/WeatherBroadcastReceiver.kt
+++ app/src/main/java/com/example/perfectweatherallyear/util/WeatherBroadcastReceiver.kt
@@ -9,6 +9,7 @@ import com.example.perfectweatherallyear.appComponent
 import com.example.perfectweatherallyear.model.DayWeather
 import com.example.perfectweatherallyear.model.Location
 import com.example.perfectweatherallyear.repository.DataResult
+import com.example.perfectweatherallyear.repository.LocationRepository
 import com.example.perfectweatherallyear.repository.WeatherRepository
 import com.example.perfectweatherallyear.ui.weekWeather.DAYS_NUMBER
 import kotlinx.coroutines.Dispatchers
@@ -19,16 +20,26 @@ import javax.inject.Inject
 
 class WeatherBroadcastReceiver : BroadcastReceiver() {
     @Inject
-    lateinit var repository: WeatherRepository
+    lateinit var weatherRepository: WeatherRepository
+
+    @Inject
+    lateinit var locationRepository: LocationRepository
 
     override fun onReceive(context: Context?, intent: Intent?) {
+        var cityName = ""
+
+        if (intent?.extras?.containsKey("CITY_NAME") == true) {
+            cityName = intent.getStringExtra("CITY_NAME").toString()
+        }
 
         context?.applicationContext?.appComponent?.inject(this)
         val pendingResult: PendingResult = goAsync()
 
         GlobalScope.launch {
             try {
-                getForecast(Location(1, "Moscow"))
+                val locationId =
+                    cityName.let { locationRepository.getLocationIdByCityName(cityName) }
+                getForecast(Location(locationId, cityName))
                 withContext(Dispatchers.Main) {
                     Toast.makeText(context, "We got forecast", Toast.LENGTH_LONG).show()
                 }
@@ -43,9 +54,10 @@ class WeatherBroadcastReceiver : BroadcastReceiver() {
     private suspend fun getForecast(location: Location): List<DayWeather> {
         var weatherForecast: List<DayWeather> = mutableListOf()
         try {
-            when ( val weatherForecast1 = repository.getWeatherForecast(location, DAYS_NUMBER)) {
+            when (val weatherForecast1 =
+                weatherRepository.getWeatherForecast(location, DAYS_NUMBER)) {
                 is DataResult.Ok -> {
-                    weatherForecast = weatherForecast1.response!!
+                    weatherForecast = weatherForecast1.response
                 }
                 is DataResult.Error ->
                     Log.e("onReceiver", "no data")
