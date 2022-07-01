3e2a8b2c5c1cb653d9dfa62a7cf5c89ee8805fdd
polinas888 <p.shcherbinina@itransition.com>
2022-07-01 11:52:23 +0300

    rename methods, handle case when loadDayWeatherForNotification() will return null, separated some code to other method

--- app/src/main/java/com/example/perfectweatherallyear/util/ForecastNotifierWorker.kt
+++ app/src/main/java/com/example/perfectweatherallyear/util/ForecastNotifierWorker.kt
@@ -6,6 +6,7 @@ import androidx.work.ListenableWorker.Result.failure
 import androidx.work.ListenableWorker.Result.success
 import com.example.perfectweatherallyear.appComponent
 import com.example.perfectweatherallyear.model.DayWeather
+import com.example.perfectweatherallyear.model.HourWeather
 import com.example.perfectweatherallyear.model.Location
 import com.example.perfectweatherallyear.repository.DataResult
 import com.example.perfectweatherallyear.repository.WeatherRepository
@@ -26,7 +27,7 @@ class ForecastNotifierWorker(appContext: Context, workerParams: WorkerParameters
 
     override suspend fun doWork(): Result {
 
-        return when (val hourlyWeatherForNotificationResult = loadHourWeatherTempForNotification()) {
+        return when (val hourlyWeatherForNotificationResult = loadHourWeatherTemp()) {
             is DataResult.Ok -> {
                 NotificationHandler.postNotification(applicationContext, hourlyWeatherForNotificationResult.response)
                 success()
@@ -38,7 +39,18 @@ class ForecastNotifierWorker(appContext: Context, workerParams: WorkerParameters
         }
     }
 
-    private suspend fun loadDayWeatherForNotification(): DayWeather? {
+    private suspend fun loadHourWeatherTemp(): DataResult<String> {
+        val dayWeather = loadDayWeather()
+
+        return if (dayWeather == null) {
+            DataResult.Error("No day weather data")
+        } else {
+            val hourlyWeatherForecast = repository.getHourlyWeather(DAYS_NUMBER, dayWeather)
+            getHourWeatherTemp(hourlyWeatherForecast)
+        }
+    }
+
+    private suspend fun loadDayWeather(): DayWeather? {
         val location = Location(1, "Moscow")
         return when (val weatherForecast = repository.getWeatherForecast(location, DAYS_NUMBER)) {
             is DataResult.Ok -> {
@@ -49,25 +61,22 @@ class ForecastNotifierWorker(appContext: Context, workerParams: WorkerParameters
         }
     }
 
-    private suspend fun loadHourWeatherTempForNotification(): DataResult<String> {
-        var hourWeatherTempForNotification = ""
-        return when (val hourlyWeatherForecast =
-            loadDayWeatherForNotification()?.let { repository.getHourlyWeather(DAYS_NUMBER, it) }) {
+    private fun getHourWeatherTemp(hourlyWeatherForecast: DataResult<List<HourWeather>>): DataResult<String> {
+        var hourWeatherTemp = ""
+
+        return when (hourlyWeatherForecast)  {
             is DataResult.Ok -> {
                 val sdf = SimpleDateFormat("HH:mm:ss")
                 val currentHour: String = sdf.format(Date()).take(2)
                 for (hourWeather in hourlyWeatherForecast.response) {
                     val hour = hourWeather.time.substringAfterLast(" ").take(2)
                     if (hour == currentHour) {
-                        hourWeatherTempForNotification = "temperature ${hourWeather.temperature}"
+                        hourWeatherTemp = "temperature ${hourWeather.temperature}"
                     }
                 }
-                DataResult.Ok(hourWeatherTempForNotification)
+                DataResult.Ok(hourWeatherTemp)
             }
             is DataResult.Error -> DataResult.Error(hourlyWeatherForecast.error)
-            else -> {
-                DataResult.Error("No hourly weather")
-            }
         }
     }
 
