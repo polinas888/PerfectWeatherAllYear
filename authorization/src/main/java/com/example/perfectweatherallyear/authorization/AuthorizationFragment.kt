690fdab08154b6ba0c9d4097976cd44c8476b24f
polinas888 <p.shcherbinina@itransition.com>
2022-08-22 16:18:12 +0300

    biometric authorization, log in by finger print

--- authorization/src/main/java/com/example/perfectweatherallyear/authorization/AuthorizationFragment.kt
+++ authorization/src/main/java/com/example/perfectweatherallyear/authorization/AuthorizationFragment.kt
@@ -1,30 +1,160 @@
 package com.example.perfectweatherallyear.authorization
 
+import android.os.Build
 import android.os.Bundle
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyPermanentlyInvalidatedException
+import android.security.keystore.KeyProperties
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
 import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavDeepLinkRequest
 import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 import com.example.perfectweatherallyear.authorization.databinding.FragmentAuthorizationBinding
-
+import java.io.IOException
+import java.security.*
+import java.security.cert.CertificateException
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.NoSuchPaddingException
+import javax.crypto.SecretKey
 
 class AuthorizationFragment : Fragment() {
     private lateinit var binding: FragmentAuthorizationBinding
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var keyStore: KeyStore
+    private lateinit var keyGenerator: KeyGenerator
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentAuthorizationBinding.inflate(layoutInflater)
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.logButton.setOnClickListener {
-            navigateAuthorizationToLocation()
+        setupKeyStoreAndKeyGenerator()
+        val cipher = setupCiphers()
+
+        biometricPrompt = createBiometricPrompt()
+        loginPressed(cipher)
+    }
+
+    private fun loginPressed(cipher: Cipher) {
+        val biometricManager = BiometricManager.from(requireContext())
+        when (biometricManager.canAuthenticate()) {
+            BiometricManager.BIOMETRIC_SUCCESS -> {
+                createKey()
+                binding.logButton.setOnClickListener(PurchaseButtonClickListener(cipher, KEY_NAME))
+            }
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE ->
+                toast("No hardware for biometric features")
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE ->
+                toast("Biometric features are currently unavailable.")
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED ->
+                toast("Please associate a biometric credential with your account.")
+            else ->
+                toast("An unknown error occurred. Please check your Biometric settings")
         }
+    }
+
+    private fun setupKeyStoreAndKeyGenerator() {
+        try {
+            keyStore = KeyStore.getInstance(ANDROID_KEY_STORE)
+        } catch (e: KeyStoreException) {
+            throw RuntimeException("Failed to get an instance of KeyStore", e)
+        }
+
+        try {
+            keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, ANDROID_KEY_STORE)
+        } catch (e: Exception) {
+            when (e) {
+                is NoSuchAlgorithmException,
+                is NoSuchProviderException ->
+                    throw RuntimeException("Failed to get an instance of KeyGenerator", e)
+                else -> throw e
+            }
+        }
+    }
+
+    private fun createKey() {
+        try {
+            keyStore.load(null)
+
+            val keyProperties = KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+            val builder = KeyGenParameterSpec.Builder(KEY_NAME, keyProperties)
+                .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                .setUserAuthenticationRequired(true)
+                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                .setInvalidatedByBiometricEnrollment(true)
+
+            keyGenerator.run {
+                init(builder.build())
+                generateKey()
+            }
+        } catch (e: Exception) {
+            when (e) {
+                is NoSuchAlgorithmException,
+                is InvalidAlgorithmParameterException,
+                is CertificateException,
+                is IOException -> throw RuntimeException(e)
+                else -> throw e
+            }
+        }
+    }
+
+    private fun setupCiphers(): Cipher {
+        val cipher: Cipher
+        try {
+            val cipherString = "${KeyProperties.KEY_ALGORITHM_AES}/${KeyProperties.BLOCK_MODE_CBC}/${KeyProperties.ENCRYPTION_PADDING_PKCS7}"
+            cipher = Cipher.getInstance(cipherString)
+        } catch (e: Exception) {
+            when (e) {
+                is NoSuchAlgorithmException,
+                is NoSuchPaddingException ->
+                    throw RuntimeException("Failed to get an instance of Cipher", e)
+                else -> throw e
+            }
+        }
+        return cipher
+    }
+
+
+    private fun createBiometricPrompt(): BiometricPrompt {
+        val executor = ContextCompat.getMainExecutor(requireContext())
+
+        val callback = object : BiometricPrompt.AuthenticationCallback() {
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                super.onAuthenticationError(errorCode, errString)
+                Log.d(TAG, "$errorCode :: $errString")
+                if (errorCode == BiometricPrompt.ERROR_NEGATIVE_BUTTON) {
+                 //TODO   loginWithPassword()
+                }
+            }
+
+            override fun onAuthenticationFailed() {
+                super.onAuthenticationFailed()
+                Log.d(TAG, "Authentication failed for an unknown reason")
+            }
+
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+                Log.d(TAG, "Authentication was successful")
+                navigateAuthorizationToLocation()
+            }
+        }
+
+        val biometricPrompt = BiometricPrompt(this, executor, callback)
+        return biometricPrompt
     }
 
     private fun navigateAuthorizationToLocation() {
@@ -39,5 +169,60 @@ class AuthorizationFragment : Fragment() {
             .build()
 
         findNavController().navigate(deepLink, navOptions)
+    }
+
+    private fun toast(message: CharSequence) = Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+
+    private fun createPromptInfo(): BiometricPrompt.PromptInfo {
+        return BiometricPrompt.PromptInfo.Builder()
+            .setTitle(getString(R.string.prompt_info_title))
+            .setSubtitle(getString(R.string.prompt_info_subtitle))
+            .setDescription(getString(R.string.prompt_info_description))
+            .setConfirmationRequired(false)
+            .setNegativeButtonText(getString(R.string.prompt_info_use_app_password))
+            .build()
+    }
+
+    private fun initCipher(cipher: Cipher, keyName: String): Boolean {
+        try {
+            keyStore.load(null)
+            cipher.init(Cipher.ENCRYPT_MODE, keyStore.getKey(keyName, null) as SecretKey)
+            return true
+        } catch (e: Exception) {
+            when (e) {
+                is KeyPermanentlyInvalidatedException -> return false
+                is KeyStoreException,
+                is CertificateException,
+                is UnrecoverableKeyException,
+                is IOException,
+                is NoSuchAlgorithmException,
+                is InvalidKeyException -> throw RuntimeException("Failed to init Cipher", e)
+                else -> throw e
+            }
+        }
+    }
+
+
+    private inner class PurchaseButtonClickListener constructor(
+        var cipher: Cipher,
+        var keyName: String
+    ) : View.OnClickListener {
+
+        override fun onClick(view: View) {
+
+            val promptInfo = createPromptInfo()
+
+            if (initCipher(cipher, keyName)) {
+                biometricPrompt.authenticate(promptInfo, BiometricPrompt.CryptoObject(cipher))
+            } else {
+             //TODO   loginWithPassword()
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "Authentification"
+        private const val ANDROID_KEY_STORE = "AndroidKeyStore"
+        private const val KEY_NAME = "default_key"
     }
 }
