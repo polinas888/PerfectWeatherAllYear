fbb93d94de894b53c06c59a34c860b0cc734edf0
polinas888 <p.shcherbinina@itransition.com>
2022-09-19 16:43:15 +0300

    custom view for update weather for 1 time

---
+++ app/src/main/java/com/example/perfectweatherallyear/util/Extensions.kt
@@ -0,0 +1,13 @@
+package com.example.perfectweatherallyear.util
+
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Rect
+
+fun Context.dpToPx(dp: Float) = this.getResources().getDisplayMetrics().density * dp
+
+fun Paint.getTextWidth(string: String): Float {
+    val rect = Rect()
+    this.getTextBounds(string, 0, string.length, rect)
+    return rect.width().toFloat()
+}
