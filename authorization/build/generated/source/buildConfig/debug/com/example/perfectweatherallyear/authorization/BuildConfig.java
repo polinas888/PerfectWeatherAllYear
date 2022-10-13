36864ca77b488f93a9176f88c0d7b4629716c374
polinas888 <p.shcherbinina@itransition.com>
2022-10-13 14:33:48 +0300

    add to git ignore

---
+++ authorization/build/generated/source/buildConfig/debug/com/example/perfectweatherallyear/authorization/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.perfectweatherallyear.authorization;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.example.perfectweatherallyear.authorization";
+  public static final String BUILD_TYPE = "debug";
+}
